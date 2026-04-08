bee089d78adc9a299801360528ee39bbc25fbc45
ibragimovruslan90-glitch <ibragimovruslan90@gmail.com>
2026-04-08 14:27:06 +0300

    trends.js

--- api/trends.js
+++ api/trends.js
@@ -2,8 +2,13 @@ export default async function handler(req, res) {
     const API_KEY = process.env.YOUTUBE_API_KEY;
 
     const channels = [
-        "UC6HKWohA11bBbyob7ieP-0g" // добавляй сюда ещё
-    ];
+    "UC6HKWohA11bBbyob7ieP-0g", // твой первый
+    "UC6n4D1sE8p2qxGn8pYtBX9w",
+    "UCkR0GY0ue02kzM0b1W3O0lw",
+    "UCwzS3xJzj9p0n8C2Vq2mRbw",
+    "UC5k3K0Yk4pR5r7E2Lk0wXkw",
+    "UC9v0l9D0v9l3Vx0F9Xl8XzA"
+];
 
     const weekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
 
