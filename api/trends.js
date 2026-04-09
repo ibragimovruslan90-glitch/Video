8912f6d08a5900761ba5c22f034afa52faeedf56
ibragimovruslan90-glitch <ibragimovruslan90@gmail.com>
2026-04-09 10:47:17 +0300

    trends.js

--- api/trends.js
+++ api/trends.js
@@ -4,11 +4,8 @@ export default async function handler(req, res) {
     const channels = [
         "UC6HKWohA11bBbyob7ieP-0g", // квинка
         "UCHVY_-jY-FayjszyX1nlGtQ", // холибам
-        "UC6n4D1sE8p2qxGn8pYtBX9w", //кошка лана
-    "UCkR0GY0ue02kzM0b1W3O0lw", //ffgtv
-    "UCwzS3xJzj9p0n8C2Vq2mRbw", //robzi
-    "UC5k3K0Yk4pR5r7E2Lk0wXkw", //over show
-    "UC9v0l9D0v9l3Vx0F9Xl8XzA" //владус
+        "UClTYGNdQTwp2w3PhOpmVIkw", //кошка лана
+    "UCUzkpCMREk2AGgZwT55JizA" //владус
     ];
 
     const weekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
